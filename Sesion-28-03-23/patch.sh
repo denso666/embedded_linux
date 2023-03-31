16b8d208e0669f9474988a5ea9de6ae9272f15ac
denso666 <developerdenso@gmail.com>
2023-03-30 19:28:58 -0600

    PATCH - file verification

--- Sesion-28-03-23/patch.sh
+++ Sesion-28-03-23/patch.sh
@@ -5,14 +5,39 @@ args=( $@ )
 SECS=$(date +%s)
 ABS_PATH=$(pwd)
 
-if [ $# -gt 0 ]; then
-    find ${args[0]} -type f &> /tmp/${SECS}_file
-   
-    if [[ $(cat /tmp/${SECS}_file) = ${args[0]} ]]; then
-		exit 0
-	else
-		exit 1
-	fi
-fi
+#
+# Helpers
+#
+function help {
+    echo "patch - apply a diff file to an original"
+    echo "Usage: patch -file -path"
+    printf "\tfile\n\t\tPatch file with relative or absolute path, use .patch extension\n"
+    printf "\tpath\n\t\tDestination repository path\n"
+}
 
-exit 1
+
+if [ $# -gt 0 ]; then
+    # Help
+    if [[ ${args[0]} == "-h" ]] || [[ ${args[0]} == "--help" ]]; then
+        help
+        exit 0
+    fi
+    
+
+    find ${args[0]} -type f &> /tmp/${SECS}_file
+
+    # File verification 
+    if [[ $(cat /tmp/${SECS}_file) != ${args[0]} ]]; then
+        rm /tmp/${SECS}_file &> /dev/null
+        echo "patch: file not found"
+        exit 1
+    fi
+    
+    # Path verification
+    # if [[ ]]
+    exit 0
+else
+    echo "patch: not .patch file provided"
+    echo "Use patch -h [--help] to see more information"
+    exit 1
+fi
