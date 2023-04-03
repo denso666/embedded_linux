edf604afc409dfb09d21a2a9c5b8e4b49167c1ad
denso666 <developerdenso@gmail.com>
2023-04-03 17:20:30 -0600

    PATCH - rename file

--- Sesion-28-03-23/patch.sh
+++ Sesion-28-03-23/patch.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
-#
-# Variables
-#
-args=($@)
-SECS=$(date +%s)
-
+ARGS=($@)
+OLD_PATH=$(pwd)
 #
 # Helpers
 #
 function help {
     echo "patch - apply a diff file to an original"
-    echo "Usage: patch -file -path"
+    echo "Usage: patch -patchfile -path"
     printf "\tfile\n\t\tPatch file with relative or absolute path, use .patch extension\n"
     printf "\tpath\n\t\tDestination repository path\n"
     exit 0
@@ -20,26 +15,34 @@ function help {
 #
 # Patch
 #
-if [ $# -gt 0 ]; then
-    # Help
-    if [[ ${args[0]} == "-h" ]] || [[ ${args[0]} == "--help" ]]; then
-        help
-    fi
- 
-    find ${args[0]} -type f &> /tmp/${SECS}_file
-
+if [ $# -eq 2 ]; then
     # File verification 
-    if [[ $(cat /tmp/${SECS}_file) != ${args[0]} ]]; then
-        rm /tmp/${SECS}_file &> /dev/null
-        echo "patch: file not found"
+    if [[ -f "${ARGS[0]}" ]]; then
+        PATCHFILE=$(realpath ${ARGS[0]})
+
+        # Path verification
+        if [[ -d "${ARGS[1]}" ]]; then
+            PATCHPATH=$(realpath ${ARGS[1]})
+
+            # Change
+            sed -i "s/^rename to \w\+\(\.[A-Za-z]{3}\)\?$/&_JCFA/" $PATCHFILE
+
+            # Apply
+            patch -d $PATCHPATH < $PATCHFILE
+            echo "patch: $PATCHFILE: correctly applied"
+            cd $OLD_PATH
+            exit 0
+        else
+            echo "patch: path not found"
+            exit 1
+        fi
+    else
+        echo "patch: patchfile not found"
         exit 1
     fi
-
-    # Patch
-    git apply ${args[0]}
-    echo "patch: ${args[0]}: correctly applied"
+elif [[ ${args[0]} == "-h" ]] || [[ ${args[0]} == "--help" ]]; then
+    help
     exit 0
-
 else
     echo "patch: too few arguments"
     echo "Try 'patch --help' for more information."
